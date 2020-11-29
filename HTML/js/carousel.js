b6784d0fd2df86410279b24912354a3dce4d74fd
ventiscenque <70634724+ventiscenque@users.noreply.github.com>
2020-11-29 22:08:07 +0530

    Add files via upload

---
+++ HTML/js/carousel.js
@@ -0,0 +1,20 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  slides[0].style.display="none";
+   slides[2].style.display="none";
+ for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+}
